9b082e5c16504ba3aa7d71585be2927fdbfd9b4d
jpchato <jpchato@gmail.com>
2020-06-16 16:47:40 -0700

    finished tasks up to create home.html template

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 # Create your views here.
-    class HomePageView(TemplateView):
-        template_name = 'home.html'
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Post
