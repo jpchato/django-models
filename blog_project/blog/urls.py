c7869c3c8a7a74703e631ace65285a00c3f4a662
jpchato <jpchato@gmail.com>
2020-06-16 22:04:26 -0700

    still no rendering from loop

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import HomePageView
+from .views import HomePageView, ListView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    
+    path('post_details/<int:pk>', HomePageView.as_view(), name='post_details' )
+
 ]
